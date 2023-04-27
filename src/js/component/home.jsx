45b750c241135715b91b09f6b435937d74aa2aac
sabonrex <118281670+sabonrex@users.noreply.github.com>
2023-04-27 21:06:32 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,79 +1,38 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import "../../styles/index.css";
 
-
-let key = 0
-
-const ToDoGen = () => {
-
-	const [itemList, setItemList] = useState([]) 
-	const [value, setValue] = useState('') 
-	const [vis, setVisible] = useState({id: null}) 
-
-
-	const removeItem = (entryId) => {
-		let newList = itemList.map(a => ({...a})) 
-		newList.splice((newList.findIndex(x => x.id === entryId)), 1) 
-		setItemList(newList)
-	}
-	
-	const valueChange = (event) => {
-		setValue(event.target.value)
-	}
-
-	const taskCounter = () => {
-		let mult = "s"
-		let tasksLeft = "no"
-		let yay = "- hooray!"
-
-		if (itemList.length == 1) {tasksLeft = 1; mult = ""; yay = ""}
-		if (itemList.length > 1) {tasksLeft = itemList.length; mult = "s"; yay = ""}
-
-		return <li className="list-group-item d-flex w-100 align-middle justify-content-between">
-			<small>{tasksLeft} task{mult} left {yay}</small>
-		</li>
-	}
-
+const Home = () => {
+	const [ inputValue, setInputValue] = useState("");
+	const [ toDos, settoDos ] = useState([]);
 	return (
-		<div className="row justify-content-center">
-			<div className="col-4">
-			<h1>ToDo list</h1>
-			<ul className="shadow list-group mt-3">
-				<input 
-					type="text"
-					value={value} 
-					className="list-group-item input-group"
-					placeholder="What do I need to do today?"
-					onChange={(event) => valueChange(event)} 
-					onKeyUp={(event) => {
-						if (event.key === "Enter" && event.target.value !== "") { 
-							setItemList([...itemList, { id: key++, listItem: event.target.value }]);
-							setValue('') 
-						}
-					}}
-				/>
-				{itemList.map(entry => 
-	
-					<li key={entry.id} 
-					onMouseOver={() => setVisible({id:(entry.id)})}
-					onMouseLeave={() => setVisible({id:null})} >
-					<b>{entry.listItem}</b>
-					<button 
-						type="button" 
-						className="custom-button" 
-						onClick={() => {removeItem(entry.id)}}
-						style={vis}
-						>
-						<FontAwesomeIcon className="custom-button" icon={faTrashCan} />
-					</button>
-				</li>
-					)}
-				{taskCounter()}
-			</ul>
+		<>
+			<div className="container-fluid row d-flex justify-content-center align-items-center p-2">
+				<h1 className="row justify-content-center">To-Do List</h1>
+				<ul className="list-container col-12">
+					<li className="list-title">
+						<input 
+						className="text" 
+						onChange={(e) => setInputValue(e.target.value)} 
+						value={inputValue}
+						onKeyPress={(e) => {
+							if (e.key === "Enter") {
+								settoDos(toDos.concat([inputValue]));
+								setInputValue("");
+							}
+						}}
+						placeholder="What do you need to do today?">
+						</input>
+					</li>
+					{toDos.map((item, index) => (
+						<li className="list-item">{item}
+						<button className="ocultar btn btn-sm btn-secondary" type="button" onClick={() => settoDos(toDos.filter((item, currentIndex) => index != currentIndex))}><i className="fas fa-trash-alt"></i></button>
+						</li>
+					))}
+				</ul>
+				<p><em>{toDos.length} item(s) left</em></p>
 			</div>
-		</div>
+		</>
 	);
 };
 
-export default ToDoGen;
+export default Home;
