97c672557a1622472dfb71538056d8cb1ac27c99
sabonrex <118281670+sabonrex@users.noreply.github.com>
2023-04-27 15:58:56 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [input, setInput] = useState(""); 
+  const [tareas, setTareas] = useState([]); 
+
+  return (
+    <div className="container">
+      <h1>To-Do List</h1>
+      <ul>
+        <li>
+        
+        <input
+  type="text"
+  onChange={(e) => setInput(e.target.value)} 
+  value={input} 
+  onKeyPress={(e) => {
+    if (e.key === "Enter" && input.length >= 3) { 
+      setTareas(tareas.concat(input));
+      setInput(""); 
+    }
+  }}
+  placeholder="Add your to-do"
+/>
+
+        </li>
+        {tareas.map((tarea, index) => ( 
+          <li>
+            <span>{tarea}</span> 
+            <i
+              className="fa-solid fa-xmark"
+              onClick={() =>
+                setTareas(
+                  tareas.filter((tarea, currentIndex) => index != currentIndex)
+                )
+              } 
+
+            ></i>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+       {tareas.length} {tareas.length >=2 ? "tareas" : "tarea"} </div> 
+    </div>
+  );
 };
 
 export default Home;
