4e76bb703cdc750ea5297160ffcc2d61d4d1d9ba
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-04-28 01:53:25 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,33 +3,32 @@ import "../../styles/index.css";
 
 const Home = () => {
 	const [ inputValue, setInputValue] = useState("");
-	const [ toDo, settoDo ] = useState([]);
+	const [ toDoItem, settoDoItem ] = useState([]);
 	return (
 		<>
 		<div className="container-fluid row d-flex justify-content-center align-items-center p-2">
-				<h1 className="row justify-content-center">To Do List</h1>
-				<ul className="list-container shadow col-12">
+			<h1 className="row justify-content-center">To Do List</h1>
+			<ul className="list-container shadow col-12">
 					<li className="list-title">
 						<input 
 						className="text" 
 						onChange={(e) => setInputValue(e.target.value)} 
 						value={inputValue}
 						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								settoDo(toDo.concat([inputValue]));
-								setInputValue("");
+						if (e.key === "Enter") {
+							settoDoItem(toDoItem.concat([inputValue]));
+							setInputValue("");
 							}
-						}}
-						placeholder="Add Task">
-						</input>
+								}}
+							placeholder="Add Task"></input>
 					</li>
-					{toDo.map((item, index) => (
+						{toDoItem.map((item, index) => (
 						<li className="list-item">{item}
-						<button className="invisible btn btn-sm btn-default" type="button" onClick={() => settoDo(toDo.filter((item, currentIndex) => index != currentIndex))}><i className="fas fa-trash-alt"></i></button>
+						<button className="invisible btn btn-sm btn-default" type="button" onClick={() => settoDoItem(toDoItem.filter((item, currentIndex) => index != currentIndex))}><i className="fas fa-trash-alt"></i></button>
 						</li>
-					))}
-				</ul>
-				<p><em>{toDo.length} task(s) left</em></p>
+						))}
+			</ul>
+				<p><em>{toDoItem.length} task(s) left</em></p>
 			</div>
 		</>
 	);
